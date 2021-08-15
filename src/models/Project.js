c264e5b89a63e6094dbf0e3046c3ea7fdfd72101
EC2 Default User <ec2-user@ip-172-31-50-101.ec2.internal>
2021-08-15 16:40:12 +0000

    Added required constraints to Project model.

--- src/models/Project.js
+++ src/models/Project.js
@@ -2,12 +2,19 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const schema = new Schema({
-    name: String,
-    description: String,
+    name: {
+        type: String,
+        required: [true, "The project name must be provided."]
+    },
+    description: {
+        type: String,
+        required: [true, "The project description must be provided."]
+    },
     priority: {
         type: Number,
         min: [0, 'Priority cannot be a negative number.'],
         max: [10, 'Priority cannot be higher then 10.'],
+        default: 0
     },
 });
 
